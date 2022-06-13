b4d6a95d5250919e31feb5993c134c7026b4070f
Askri Ghassen <askrighassem@gmail.com>
2022-06-13 12:18:06 +0200

    footer logo should redirect to home

--- components/footer.vue
+++ components/footer.vue
@@ -1,18 +1,20 @@
 
 <template>
     <footer class="footer has-background-light">
-         <img class ="logo content" src="https://la-combe-du-lion-vert.fr/wp-content/themes/la-combe-du-lion-vert/images/logo.svg">
-        <div class="content has-text-centered">
-            &copy;
-            <br>
-            <small>A Software Crafter's Journey</small> <strong>x</strong> <small>La Combe du Lion Vert</small>
-        </div>
+        <a href="/">
+            <img class ="logo-footer" src="https://la-combe-du-lion-vert.fr/wp-content/themes/la-combe-du-lion-vert/images/logo.svg">
+                <div class="content has-text-centered">
+                &copy;
+                <br>
+                <small>A Software Crafter's Journey</small> <strong>x</strong> <small>La Combe du Lion Vert</small>
+            </div>
+        </a>
     </footer>
 </template>
 <style scoped>
  .logo-footer{
-    width:100px; 
-    height:100px;
+    width: 100px;
+    height: 100px;
     text-align:center;
     margin:auto;
     display:flex;
